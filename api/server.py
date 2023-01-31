6735039d0235be647257ba2dcc4f0f6085465fa6
Cyber-Elu <tony.stonem77@gmail.com>
2023-01-31 15:04:03 +0100

    commit 2

--- api/server.py
+++ api/server.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import pandas as pd
+
+df = pd.read_csv('assets/authors.csv')
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/authors/<int:id>")
+def author(id):
+    return df.iloc(id)
+
 
 if __name__ == "__main__" :
     app.run(debug=True, host='0.0.0.0')
