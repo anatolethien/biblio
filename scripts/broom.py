8478a300084522b8483e0e4e83a9bb2d075d104c
anatolethien <mail@anatolethien.eu>
2023-02-07 16:34:22 +0100

    only books

--- scripts/broom.py
+++ scripts/broom.py
@@ -33,10 +33,11 @@ df_books = df_books.dropna()
 df_books = df_books.loc[df_books['lang'] == 'en']
 df_books['bestsellers-rank'] = df_books['bestsellers-rank'].astype(np.int32)
 df_books['format'] = df_books['format'].astype(np.int32)
+df_books = df_books.loc[df_books['format'].isin([1,2,9,16,22])]
 df_books['publication-date'] = pd.to_datetime(df_books['publication-date']).dt.date
 df_books['rating-count'] = df_books['rating-count'].astype(np.int32)
 
 print(df_books.info(memory_usage='deep'))
 
-# df_books.sample(5000).to_csv('assets/books.csv', index=False)
-df_books.to_csv('assets/books.csv', index=False),
+df_books.sample(5000).to_csv('assets/books.csv', index=False)
+# df_books.to_csv('assets/books.csv', index=False),
