c6a6deb14574ba96a75afe75004a7165e7bfeac3
anatolethien <mail@anatolethien.eu>
2023-02-16 14:19:50 +0100

    single quote refactoring

--- scripts/broom.py
+++ scripts/broom.py
@@ -6,7 +6,7 @@ import numpy as np
 def books_broom():
     origin_path = 'downloads/dataset.csv'
     destination_path = 'assets/books.csv'
-    print(f"Reading the books dataset from {origin_path}...")
+    print(f'Reading the books dataset from {origin_path}...')
     df_books = pd.read_csv(
         filepath_or_buffer=origin_path,
         usecols=[
@@ -30,7 +30,7 @@ def books_broom():
             'weight'
         ]
     )
-    print("Treating data...")
+    print('Treating data...')
     df_books = df_books.dropna()
     correct_langs = ['en']
     df_books = df_books.loc[df_books['lang'].isin(correct_langs)]
@@ -40,7 +40,7 @@ def books_broom():
     df_books = df_books.loc[df_books['format'].isin(correct_formats)]
     df_books['publication-date'] = pd.to_datetime(df_books['publication-date']).dt.date
     df_books['rating-count'] = df_books['rating-count'].astype(np.int32)
-    print(f"Writing the books dataset to {destination_path}...")
+    print(f'Writing the books dataset to {destination_path}...')
     df_books.sample(5000).to_csv(
         path_or_buf=destination_path,
         index=False
@@ -49,18 +49,18 @@ def books_broom():
 def authors_broom():
     origin_path = 'downloads/authors.csv'
     destination_path = 'assets/authors.csv'
-    print(f"Reading the authors dataset from {origin_path}...")
+    print(f'Reading the authors dataset from {origin_path}...')
     df_authors = pd.read_csv(
         filepath_or_buffer=origin_path
     )
-    print("Treating data...")
+    print('Treating data...')
     df_authors = df_authors.rename(
         {
             'author_id': 'id',
             'author_name': 'name'
         }
     )
-    print(f"Writing the books dataset to {destination_path}...")
+    print(f'Writing the books dataset to {destination_path}...')
     df_authors.to_csv(
         path_or_buf=destination_path,
         index=False
@@ -69,18 +69,18 @@ def authors_broom():
 def categories_broom():
     origin_path = 'downloads/categories.csv'
     destination_path = 'assets/categories.csv'
-    print(f"Reading the categories dataset from {origin_path}...")
+    print(f'Reading the categories dataset from {origin_path}...')
     df_categories = pd.read_csv(
         filepath_or_buffer=origin_path
     )
-    print("Treating data...")
+    print('Treating data...')
     df_categories = df_categories.rename(
         {
             'category_id': 'id',
             'category_name': 'name'
         }
     )
-    print(f"Writing the categories dataset to {destination_path}...")
+    print(f'Writing the categories dataset to {destination_path}...')
     df_categories.to_csv(
         path_or_buf=destination_path,
         index=False
@@ -89,11 +89,11 @@ def categories_broom():
 def formats_broom():
     origin_path = 'downloads/formats.csv'
     destination_path = 'assets/formats.csv'
-    print(f"Reading the formats dataset from {origin_path}...")
+    print(f'Reading the formats dataset from {origin_path}...')
     df_formats = pd.read_csv(
         filepath_or_buffer=origin_path
     )
-    print("Treating data...")
+    print('Treating data...')
     df_formats.rename(
         columns={
             'format_id': 'id',
@@ -105,16 +105,16 @@ def formats_broom():
         by='id',
         ascending=True
     )
-    print(f"Writing the formats dataset to {destination_path}...")
+    print(f'Writing the formats dataset to {destination_path}...')
     df_formats.to_csv(
         path_or_buf=destination_path,
         index=False
     )
 
 if __name__ == '__main__' :
-    print("Broom!")
+    print('Broom!')
     books_broom()
     authors_broom()
     categories_broom()
     formats_broom()
-    print("Done!")
+    print('Done!')
