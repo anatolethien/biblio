15aea366e0d1d629acf15a0c845881f816f96bc0
anatolethien <mail@anatolethien.eu>
2023-05-23 03:08:48 +0200

    rush

--- app.py
+++ app.py
@@ -1,33 +1,71 @@
-from flask import Flask, request, session, redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, session
+from flask_security import Security, SQLAlchemyUserDatastore, \
+    login_required, login_user, logout_user, current_user
+
+from database import db_session, init_db
+from models import User, Role
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'secr3t'
+
+user_datastore = SQLAlchemyUserDatastore(db_session, User, Role)
+security = Security(app, user_datastore)
+
+
+@app.before_first_request
+def create_user():
+    init_db()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    from user import User
-    if request.method == 'GET':
-        return render_template('register.html')
-    elif request.method == 'POST':
-        db.session.add(User(username=request.form['username'], password=request.form['password']))
-        db.session.commit()
-        return redirect('/login')
+    match request.method:
+        case 'GET':
+            return render_template('register.html')
+        case 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            user_datastore.create_user(username=username, password=password)
+            db_session.commit()
+            return redirect('/login')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    from user import User
-    if request.method == 'GET':
-        return render_template('login.html')
-    elif request.method == 'POST':
-        pass
+    match request.method:
+        case 'GET':
+            return render_template('login.html')
+        case 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            user = user_datastore.get_user(username)
+            if user and user_datastore.verify_password(password, user):
+                login_user(user)
+                return redirect('/')
+            else:
+                return redirect('/error')
+            pass
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', current_user=current_user)
+
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import UserMixin, RoleMixin
-from sqlalchemy import Boolean, DateTime, Column, Integer, String, ForeignKey
+from sqlalchemy import Boolean, Column, Integer, String
 
 from database import Base
 
@@ -8,12 +8,14 @@ db = SQLAlchemy()
 
 class User(Base, UserMixin):
     __tablename__ = 'user'
-    id = Column(Integer(), primary_key=True)
+    id = Column(Integer, primary_key=True)
     username = Column(String(255), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
+    email = Column(String(255), unique=True)
+    active = Column(Boolean)
 
 class Role(Base, RoleMixin):
     __tablename__ = 'role'
-    id = Column(Integer(), primary_key=True)
-    name = Column(String(80), unique=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), unique=True)
     description = Column(String(255))
